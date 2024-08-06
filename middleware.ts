14988164ef1155eec92133aeca423854553f0cfc
Franco <francobetteo@gmail.com>
2024-08-05 23:06:22 -0300

    typescripts

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from 'next/server';
 import { getSession } from '@auth0/nextjs-auth0';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { IncomingMessage, ServerResponse } from 'http';
 
-export async function middleware(req) {
-  const res = NextResponse.next();
+export async function middleware(req: NextApiRequest, res: NextApiResponse) {
   const session = await getSession(req, res);
 
   if (!session) {
