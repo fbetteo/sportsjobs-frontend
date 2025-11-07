79c2e9c5ee1e3819d6b42594b43e8a36d4d603e9
fbetteo <francobetteo@gmail.com>
2025-11-07 11:11:02 -0300

    modal message in cancellation

--- components/ConfirmCancelModal.tsx
+++ components/ConfirmCancelModal.tsx
@@ -1,4 +1,4 @@
-import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter } from '@chakra-ui/react';
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter, Box } from '@chakra-ui/react';
 
 interface ConfirmCancelModalProps {
     isOpen: boolean;
@@ -13,7 +13,12 @@ const ConfirmCancelModal = ({ isOpen, onClose, onConfirm }: ConfirmCancelModalPr
             <ModalContent bg="purple.700" color="white">
                 <ModalHeader>Confirm Cancellation</ModalHeader>
                 <ModalBody>
-                    Are you sure you want to cancel your subscription? This will immediately remove access to jobs.
+                    <Box mb={4}>
+                        Are you sure you want to cancel your subscription? This will immediately remove access to jobs.
+                    </Box>
+                    <Box fontSize="sm" color="gray.200">
+                        If you encounter any issues, please contact support at franco@sportsjobs.online
+                    </Box>
                 </ModalBody>
                 <ModalFooter>
                     <Button bg="green.400" color="white" _hover={{ bg: 'green.500' }} _active={{ bg: 'green.600' }} mr={3} onClick={onClose}>
